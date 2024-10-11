4642d368d90d03d30d7adce0adc45f527d0e3456
Mario Mendonca <mariomeendonca00@gmail.com>
2024-10-11 09:32:16 -0300

    fix

--- src/pages/CarListPage.tsx
+++ src/pages/CarListPage.tsx
@@ -1,20 +1,13 @@
-import { useState } from 'react';
 import { CarList } from '../components/CarList';
 import { useNavigate } from 'react-router-dom'
 import { carsData } from '../database'
 
 export const CarListPage = () => {
   const navigate = useNavigate();
-  const [cars] = useState(carsData);
-
-  const handleSelectCar = (carId: number) => {
-    handleNavigateToDetails(carId);
-  };
-
 
   const handleNavigateToDetails = (carId: number) => {
     navigate(`/car/${carId}`);
   };
 
-  return <CarList cars={cars} onSelectCar={handleSelectCar} />;
+  return <CarList cars={carsData} onSelectCar={handleNavigateToDetails} />;
 };
